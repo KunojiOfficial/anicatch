6a3f3a32596883d9275531a8efff48196e25576f
Kunoji <kunojigames@gmail.com>
2025-03-21 13:02:23 +0100

    .env production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  envDir: './',
-  server: {
-    allowedHosts: ["play.anicatch.com"],
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    envDir: './',
+    define: {
+      'process.env': env, // Ensures variables are available in runtime
     },
-    hmr: {
-      clientPort: 443,
-    }
-  },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+    server: {
+      allowedHosts: ["play.anicatch.com"],
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+      hmr: {
+        clientPort: 443,
+      }
+    },
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+  }
+});
