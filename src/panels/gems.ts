6140ebeffd7d5b7261314a23a35661bfe6c45415
Kunoji <kunojigames@gmail.com>
2025-04-25 21:58:47 +0200

    Gems buy buttons

--- src/panels/gems.ts
+++ src/panels/gems.ts
@@ -2,10 +2,22 @@ import { InteractionReplyOptions } from "discord.js";
 import { DiscordInteraction } from "../types.ts";
 import Panel from "../classes/Panel.ts";
 
+import entitlements from "../config/entitlements.json";
+
 export default new Panel({
     name: "gems",
     async execute(interaction: DiscordInteraction): Promise<InteractionReplyOptions> {
         const { user, client } = interaction;
+
+        const gems = entitlements.filter(e => e.type === "consumable" && e.rewards?.find(r => r.type === "gems"));
+        const buttons = [[]];
+        for (const gem of gems) {
+            if (buttons[buttons.length-1].length >= 5) buttons.push([]);
+            buttons[buttons.length-1].push({
+                style: "premium",
+                skuId: gem.id
+            });
+        }
 
         return { 
             embeds: [ interaction.components.embed({
@@ -13,11 +25,7 @@ export default new Panel({
                 description: `{locale_main_gemStoreText}`,
                 thumbnail: client.getEmojiUrl("gem")
             }) ],
-            components: [ interaction.components.buttons([{
-                label: "{locale_main_buyGems}",
-                url: "{config_urls_store}",
-                emoji: "smallGem"
-            }]) ]
+            components: buttons.map(b => interaction.components.buttons(b))
         }
     }
 }); 
